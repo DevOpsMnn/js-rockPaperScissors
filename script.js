ae7824a5ec37c11ded74a8471454951bc973ed08
DevOpsMnn <tim.moonen@proton.me>
2022-08-26 23:13:58 +0200

    Created boneplate for HTML and coding idea in JS file

---
+++ script.js
@@ -0,0 +1,12 @@
+/************************************************************************************************
+Make a variable to give the computer player a hand
+Make a if statement to determine who wins. 
+This should have:
+    Tie, try again:
+    Comp rock, player paper
+    Comp rock, player scissors
+    Comp scissors, player paper
+    Comp scissors, player rock
+    Comp paper, player rock
+    Comp paper, player scissors
+*************************************************************************************************/
