1453a5a862360c64513b126eb2ee4d3413c8ef0a
DevOpsMnn <tim.moonen@proton.me>
2022-08-31 22:05:30 +0200

    Def need to get back to this, but it works finally!

--- script.js
+++ script.js
@@ -19,83 +19,85 @@ gets to 5 wins winst the game.
 
 
 *************************************************************************************************/
-// Variables
-let playerSelection = getPlayerChoice();
-const computerChoice = getComputerChoice();
-let playerScore = 0;
-let computerScore = 0;
-
 // Functions
 // Create a random choice for the computer player
 function getComputerChoice() {
-  const computerChoice = Math.round(Math.random() * 2 + 1);
+  let computerChoice = Math.round(Math.random() * 3);
   if (computerChoice == 1) {
-    return "paper";
+    computerChoice = "paper";
+    return computerChoice;
   } else if (computerChoice == 2) {
-    return "rock";
+    computerChoice = "rock";
+    return computerChoice;
   } else {
-    return "scissors";
+    computerChoice = "scissors";
+    return computerChoice;
   }
 }
 
 //Allow player to choose a hand and validates the input
 function getPlayerChoice() {
   let askHand = prompt("What hand do you want to play? Rock, Paper or scissors?");
-  let playerSelection = askHand.toLowerCase();
-  if (playerSelection == "paper") {
-        return playerSelection;}
-    else if (playerSelection == "rock") {
-        return playerSelection;}
-    else if (playerSelection == "scissors") {
-        return playerSelection}
-    else {
-        console.log("Thats not a valid input")
-    }    
+  playerSelection = askHand.toLowerCase();
+  return playerSelection;   
 }
 
 //Create a function that playes a round
 function playRound(playerSelection, computerChoice) {
+    let result = "";
         if (computerChoice == "scissors" && playerSelection == "paper") 
         {
-            console.log("You lose! " + computerChoice + " wins from " + playerSelection + "!");
-            computerScore ++;
-            console.log("Computer score: " + computerScore + " - Player Score: " + playerScore + "!");
+            result = 0;
         } 
         else if (computerChoice == "scissors" && playerSelection == "rock") 
         {
-            console.log("You win! " + computerChoice + " wins from " + playerSelection + "!");
-            playerScore ++;
-            console.log("Computer score: " + computerScore + " - Player Score: " + playerScore + "!");
+            result = 1;
         } 
         else if (computerChoice == "paper" && playerSelection == "rock") 
         {
-            console.log("You lose! " + computerChoice + " wins from " + playerSelection + "!");
-            computerScore ++;
-            console.log("Computer score: " + computerScore + " - Player Score: " + playerScore + "!");    
+            result = 0;    
         } 
         else if (computerChoice == "paper" && playerSelection == "scissors") 
         {
-            console.log("You win! " + computerChoice + " looses from " + playerSelection + "!");
-            playerScore ++;
-            console.log("Computer score: " + computerScore + " - Player Score: " + playerScore + "!");
+            result = 1;
         } 
         else if (computerChoice == "rock" && playerSelection == "paper")
         {
-            console.log("You win! " + computerChoice + " looses from " + playerSelection + "!");
-            playerScore ++;
-            console.log("Computer score: " + computerScore + " - Player Score: " + playerScore + "!");
+            result = 1;
         } 
         else if (computerChoice == "rock" && playerSelection == "scissors") 
         {
-            console.log("You lose! " + computerChoice + " wins from " + playerSelection + "!");
-            computerScore ++;
-            console.log("Computer score: " + computerScore + " - Player Score: " + playerScore + "!");
+            result = 0;
         } 
         else 
         { 
-            console.log("Draw! Try again!");
+            result = 2;
         }
+        return result;
     }
 
+function game() {
+    let computerChoice = "";
+    let playerSelection = "";
+    let playerScore = 0;
+    let computerScore = 0;
+    let playedRound = "";
 
-playRound(playerSelection, computerChoice);
+    for (var i = 0; i < 5; i++){
+        playerSelection = getPlayerChoice();
+        computerChoice = getComputerChoice();
+        playedRound = playRound(playerSelection, computerChoice);
+        if (playedRound == 2) {
+            console.log("Draw!")
+        } else if (playedRound == 1) {
+            playerScore ++;
+        } else {
+            computerScore++;
+        }
+        console.log(playedRound);
+        console.log(computerChoice, playerSelection);
+        console.log("Computer score: " + computerScore + " - Player Score: " + playerScore + "!");
+    }
+};
+
+game();
