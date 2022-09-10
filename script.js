cfceb02b63d8d71b988274d9fc5e9e4a92a13238
DevOpsMnn <tim.moonen@proton.me>
2022-09-10 17:23:49 +0200

    Struggleing again on the 5 rounds part...

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     game(button.id);
   });
 });
 
-// Create a random choice for the computer player
-function getComputerChoice() {
+function playRound(e){
   let computerChoice = Math.round(Math.random() * 3);
+  let playedRound
   if (computerChoice == 1) {
-    computerChoice = "paper";
+      computerChoice = "paper";
   } else if (computerChoice == 2) {
-    computerChoice = "rock";
+      computerChoice = "rock";
   } else {
-    computerChoice = "scissors";
+      computerChoice = "scissors";
   }
-  return computerChoice;
-}
-
-//Create a function that playes a round
-function playRound(playerSelection, computerChoice) {
-  let result = "";
-  if (computerChoice == "scissors" && playerSelection == "paper") {
-    result = 0;
-  } else if (computerChoice == "scissors" && playerSelection == "rock") {
-    result = 1;
-  } else if (computerChoice == "paper" && playerSelection == "rock") {
-    result = 0;
-  } else if (computerChoice == "paper" && playerSelection == "scissors") {
-    result = 1;
-  } else if (computerChoice == "rock" && playerSelection == "paper") {
-    result = 1;
-  } else if (computerChoice == "rock" && playerSelection == "scissors") {
-    result = 0;
-  } else {
-    result = 2;
-  }
-  return result;
-}
-
-function game(e) {
-  let computerChoice = getComputerChoice();
-  let playedRound = playRound(e, computerChoice);
+  if (computerChoice == "scissors" && e == "paper") {
+    playedRound = 0;
+    } else if (computerChoice == "scissors" && e == "rock") {
+      playedRound = 1;
+    } else if (computerChoice == "paper" && e == "rock") {
+      playedRound = 0;
+    } else if (computerChoice == "paper" && e == "scissors") {
+      playedRound = 1;
+    } else if (computerChoice == "rock" && e == "paper") {
+      playedRound = 1;
+    } else if (computerChoice == "rock" && e == "scissors") {
+      playedRound = 0;
+    } else {
+      playedRound = 2;
+    }
   console.log("Computer chooses " + computerChoice + " and player chooses " + e + ".");
   if (playedRound == 2) {
-    document.getElementById("results").innerHTML = "You played " + e + " and the computer went for " + computerChoice + " It's a draw!";
+      document.getElementById("results").innerHTML =
+      "You played " + e + " and the computer went for " + computerChoice + " It's a draw!";
   } else if (playedRound == 1) {
-    document.getElementById("results").innerHTML =  "You played " + e + " and the computer went for " + computerChoice +
-    "! You Win!";;
+      document.getElementById("results").innerHTML =
+      "You played " + e + " and the computer went for " + computerChoice + "! You Win!";
   } else {
-    document.getElementById("results").textContent = "You played " + e + " and the computer went for " + computerChoice +
-     "! Computer Wins!";
+      document.getElementById("results").textContent =
+      "You played " + e + " and the computer went for " + computerChoice + "! Computer Wins!";
+  }
+}
+
+function game(e){
+  let playerScore =  0;
+  let computerScore = 0;
+  if (playerScore = 3 ){    
+    console.log("Player Wins!", playerScore, computerScore);
+  } else if (computerScore = 3) {
+    console.log("Computer Wins!", playerScore, computerScore);
+  } else {
+    playRound(e);
   }
 }
